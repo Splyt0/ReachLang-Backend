3625e5da5ae5559975fd919c0541383b20ab22b4
GigaHierz <lenahierzi@hotmail.de>
2022-06-25 22:33:05 -0400

    Add hackathon reach file

--- hackathon.rsh
+++ hackathon.rsh
@@ -4,13 +4,13 @@
 
 export const main = Reach.App(() => {
 
-const ExpenseCreator = Participant("Payer", {
+const ExpenseCreator = Participant("Winner", {
     amount: UInt,
-    payees: Array(Address, 5),
+    winners: Array(Address, 5),
 
 });
 
-const SettleExpense = API("Settle", {
+const SettleWinnings = API("Settle", {
     settleUp : Fun([], Bool),
 });
 
@@ -18,21 +18,24 @@ init();
 
 ExpenseCreator.only(() => {
     const amount = declassify(interact.amount);
-    const payees = declassify(interact.payees);
+    const winners = declassify(interact.winners);
 });
 
-ExpenseCreator.publish(amount,payees);
+ExpenseCreator.publish(amount,winners);
 
-const [totalPaid] = parallelReduce([amount/5 ])
+const [totalPaid] = parallelReduce([ amount/5 ])
   .invariant(balance() == 0)
-  .while(totalPaid > amount/5 * 4)
-  .api_(SettleExpense.settleUp, () => {
+  .while(totalPaid > amount/5)
+  .api_(SettleWinnings.settleUp, () => {
     return[
         [amount/5],
         notify => {
-            transfer(amount/5).to(payees[]);
+            winners.forEach((winner) => {
+            transfer(amount/5).to(winner);
+
+            })
             notify(true);
-            return [false]
+            return [totalPaid -  amount/5 * 5 ]
         }
     ]       
 });
--- index.mjs
+++ index.mjs
@@ -1,30 +1,20 @@
-import * as splitOO from './build/expenses.main.mjs'
+import * as expenses from './build/expenses.main.mjs'
+import * as hackathon from './build/hackathon.main.mjs'
 import { loadStdlib, ask } from '@reach-sh/stdlib'
 
 const stdlib = await loadStdlib('ETH')
 stdlib.setProviderByName('TestNet')
 
-// const bal = stdlib.parseCurrency(1000)
-// const acct = await stdlib.newTestAccount(bal)
-
-// const getAccFromMnemonic = async (
-//   message = 'Please paste the mnemonic of the deployer:'
-// ) => {
-//   const mnemonic = await ask.ask(message);
-//   const fmtMnemonic = mnemonic.replace(/,/g, '');
-//   const acc = await stdlib.newAccountFromMnemonic(fmtMnemonic);
-//   return acc;
-// };
-
 const acct = await stdlib.newAccountFromMnemonic(
   'act eyebrow discover post pond seek cart scout toward arrive language rebel'
 )
 console.log(acct)
-const ctc = acct.contract(splitOO)
+const ctcExpenses = acct.contract(expenses)
+const ctcHackathon = acct.contract(hackathon)
 
-// deploy contract
+// deploy Expenses contract
 await stdlib.withDisconnect(() =>
-  ctc.p.Payer({
+  ctcExpenses.p.Payer({
     amount: 1,
     payees: [
       '0xcb270e8610977301c85d39edb608b5513bbf6261',
@@ -35,10 +25,25 @@ await stdlib.withDisconnect(() =>
     ]
   })
 )
+// deploy Hackathon contract
+await stdlib.withDisconnect(() =>
+  ctcHackathon.p.Winner({
+    amount: 100,
+    winners: [
+      '0xcb270e8610977301c85d39edb608b5513bbf6261',
+      '0xef0d7077b93af44b966f2a2469715323ac4966e7',
+      '0xef0d7077b93af44b966f2a2469715323ac4966e6',
+      '0xef0d7077b93af44b966f2a2469715323ac4966e5',
+      '0xef0d7077b93af44b966f2a2469715323ac4966e4'
+    ]
+  })
+)
 
 //Returns the contract address - getInfo
-const splitOOCtcAddress = await ctc.getInfo()
+const expensesCtcAddress = await ctcExpenses.getInfo()
+const hackathonCtcAddress = await ctcHackathon.getInfo()
 
 console.log('********************************************')
-console.log('Announcer contract address is:', splitOOCtcAddress)
+console.log('Announcer contract address is:', expensesCtcAddress)
+console.log('Announcer contract address is:', hackathonCtcAddress)
 console.log('********************************************')
